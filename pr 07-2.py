938b8575142ef2fbd562e57a1125f00992b3f4ae
sundong1915 <sundong1915@gmail.com>
2026-03-12 22:53:15 +0900

    pr 07-2

---
+++ pr 07-2.py
@@ -0,0 +1,12 @@
+def main():
+    num=input("input: ")
+    if num.isdigit():
+        num3=int(num)
+        num2=num3**2
+        print(num2)
+    
+    else:
+        print("input only numbers")
+
+
+main()
