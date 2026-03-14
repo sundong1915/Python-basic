fc95d60698eb75df50542d764c28fea856d3916c
sundong1915 <sundong1915@gmail.com>
2026-03-14 23:50:25 +0900

    pr 08-2

---
+++ pr 08-2.py
@@ -0,0 +1,18 @@
+cm=0
+
+while True:
+    cm+=1
+    if cm%6==0 and cm%45==0:
+      print(cm)
+      break
+
+num1=42
+num2=120
+i=num1
+
+while True:
+   i-=1
+   if num1%i==0 and num2%i==0:
+      print(i)
+      break
+
