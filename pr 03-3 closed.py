c41d132f3ac21cf215764b216f065e06e5fee41c
sundong1915 <sundong1915@gmail.com>
2026-04-02 14:16:56 +0900

    pr 03-3 closed

---
+++ pr 03-3 closed.py
@@ -0,0 +1,21 @@
+sum=0
+
+for i in range(1,10,2):
+ sum+=i
+
+print(sum)
+
+mul=1
+
+for j in range(1,11):
+ mul*=j
+
+print(mul)
+
+sm=1
+
+for k in range(1,10):
+ print(k*7)
+
+for l in range(63,6,-7):
+ print(l)
