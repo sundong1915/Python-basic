a545dbdbfe1f5ee59cb90191cd2245d993de465e
sundong1915 <sundong1915@gmail.com>
2026-04-09 23:50:19 +0900

    pr 05-3 closed

---
+++ pr 05-3 closed.py
@@ -0,0 +1,17 @@
+# pr 05-2 by mistake
+
+st=[1,2,3,4,5]
+st[1:4]=[3]
+print(st)
+
+st[1:2]=[3,3.5]
+print(st)
+
+st[2:3]=[]
+print(st)
+
+st1=[1,2,3,4,5,6,7,8,9,10]
+st2=st1[0:9:2]
+print(st2)
+st3=st1[1:11:2]
+print(st3)
