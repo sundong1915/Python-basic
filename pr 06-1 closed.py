074d903890abbed9e50de53a1f7813054fa53b3b
sundong1915 <sundong1915@gmail.com>
2026-04-11 22:51:57 +0900

    pr 06-1 closed

---
+++ pr 06-1 closed.py
@@ -0,0 +1,23 @@
+st=[]
+st.append(1)
+print(st)
+
+st.remove(1)
+print(st)
+
+st2=[1,2,3]
+st2.pop(1)
+print(st2)
+
+st3=[1,2,3,4]
+st3=[]
+print(st3)
+
+st4=[]
+for i in range(1,10):
+    st4.append(i)
+print(st4)
+
+st5=[1,2]
+st5[2:]=[3,4,5]
+print(st5)
